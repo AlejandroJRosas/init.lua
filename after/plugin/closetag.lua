0dcc72f901008f32c92a66c6e0bb6cd0683fc41d
Alejandro <alejandrojrosas2001@gmail.com>
2024-01-22 20:27:28 -0400

    added some qol remaps

--- after/plugin/closetag.lua
+++ after/plugin/closetag.lua
@@ -1,6 +1,7 @@
-require 'vim-closetag'.setup {
-  closetag_filenames = '*.html,*.xhtml,*.phtml,*.svelte',
-  closetag_emptyTags_caseSensitive = 1,
-  closetag_shortcut = '>',
-  closetag_close_shortcut = '<leader>>'
-}
+-- require 'vim-closetag'.setup {
+--   closetag_filenames = '*.html,*.xhtml,*.phtml,*.svelte',
+--   closetag_emptyTags_caseSensitive = 1,
+--   closetag_shortcut = '>',
+--   closetag_close_shortcut = '<leader>>'
+-- }
+-- 
--- lua/ajrosas/remap.lua
+++ lua/ajrosas/remap.lua
@@ -1,8 +1,8 @@
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
-vim.keymap.set('v', 'J', ':m ">+1<CR>gv=gv')
-vim.keymap.set('v', 'K', ':m "<-2<CR>gv=gv')
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
 vim.keymap.set('n', 'J', 'mzJ`z')
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
@@ -38,8 +38,9 @@ vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
 vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
 vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
 
+vim.keymap.set('v', '<leader>r', [[:'<,'>norm i]])
+vim.keymap.set('n', '<leader>r', [[:%s///g<Left><Left><Left>]])
 vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
 
 vim.keymap.set('n', '<leader>vpp', '<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>');
 vim.keymap.set('n', '<leader>mr', '<cmd>CellularAutomaton make_it_rain<CR>');
